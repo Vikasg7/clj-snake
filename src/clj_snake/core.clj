d14dfb623b4874ea97c03eda9ca80386c5b77367
Vikas <v33vikasgautam@gmail.com>
2021-10-04 03:46:09 +0530

    Finished 'directions'

--- src/clj_snake/core.clj
+++ src/clj_snake/core.clj
@@ -1,13 +1,14 @@
 (ns clj-snake.core
   (:import [org.jline.terminal TerminalBuilder Terminal]
            [org.jline.keymap   BindingReader]
-           [io.reactivex.rxjava3.core Observable Scheduler ObservableEmitter Scheduler$Worker]
+           [io.reactivex.rxjava3.core Observable Scheduler ObservableEmitter]
            [io.reactivex.rxjava3.schedulers Schedulers]
            [io.reactivex.rxjava3.disposables Disposable]
            [java.util.concurrent TimeUnit])
   (:require [rx-clojure.statics   :as rx]
             [rx-clojure.operators :as op]
-            [rx-clojure.functions :as fns]))
+            [rx-clojure.functions :as fns]
+            [clojure.core.match :refer [match]]))
 
 (set! *warn-on-reflection* true)
 
@@ -17,7 +18,6 @@
       (.build)))
 
 (defn char-reader ^BindingReader [^Terminal terminal]
-  (-> terminal .enterRawMode)
   (BindingReader. (-> terminal .reader)))
 
 (def ^BindingReader key-reader (char-reader (terminal)))
@@ -36,32 +36,37 @@
     (repeat-latest-on-interval source delay unit (Schedulers/io)))
   ([source delay unit ^Scheduler scheduler]
     (let [sub (fn [^ObservableEmitter e]
-                (let [dsp (atom nil)
+                (let [dsp (atom (Disposable/empty))
                       nxt (fn nxt [value]
                             (when (not (.isDisposed e))
                               (.onNext e value)
-                              (when-let [d ^Disposable @dsp] (.dispose d))
+                              (.dispose ^Disposable @dsp)
                               (reset! dsp (.scheduleDirect scheduler (fns/runnable #(nxt value)) delay unit))))
                       err (fn err [error]
-                            (when-let [d ^Disposable @dsp] (.dispose d))
+                            (.dispose ^Disposable @dsp)
                             (.onError e error))
                       com (fn com []
-                            (when-let [d ^Disposable @dsp] (.dispose d))
+                            (.dispose ^Disposable @dsp)
                             (.onComplete e))]
                 (-> source (op/subscribe nxt err com))))]
     (-> Observable (rx/create sub)))))
 
+(defn filter-opposite [prev curr]
+  (match [prev curr]
+    [\w \s] \w
+    [\s \w] \s
+    [\a \d] \a
+    [\d \a] \d
+    :else   curr))
+
 (def directions
   (-> Observable (rx/fromIterable (repeatedly key-inputs))
-      (op/startWithItem "r")
+      (op/filter #{\w \a \s \d})
+      (op/scan filter-opposite)
       (op/distinctUntilChanged)
       (op/compose to-async)))
 
 (defn -main [& args]
   (-> directions
-      (repeat-latest-on-interval 1000 TimeUnit/MILLISECONDS)
-      (op/blockingSubscribe println)))
-
-;; (dorun (->> (repeatedly key-inputs)
-;;             (take 10)
-;;             (map println)))
+      (repeat-latest-on-interval 750 TimeUnit/MILLISECONDS)
+      (op/blockingSubscribe println println)))
