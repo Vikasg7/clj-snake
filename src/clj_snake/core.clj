3529fabf21f3874b08327d4a71423c4d3758a884
Vikas <v33vikasgautam@gmail.com>
2021-10-07 18:44:45 +0530

    Fixed food spawn bug and a lot of refactoring.

--- src/clj_snake/core.clj
+++ src/clj_snake/core.clj
@@ -1,57 +1,13 @@
 (ns clj-snake.core
-  (:import [org.jline.terminal TerminalBuilder Terminal]
-           [org.jline.keymap   BindingReader]
-           [io.reactivex.rxjava3.core Observable Scheduler ObservableEmitter]
-           [io.reactivex.rxjava3.schedulers Schedulers]
-           [io.reactivex.rxjava3.disposables Disposable]
+  (:import [io.reactivex.rxjava3.core Observable]
            [java.util.concurrent TimeUnit])
   (:require [rx-clojure.statics   :as rx]
             [rx-clojure.operators :as op]
-            [rx-clojure.functions :as fns]
             [clojure.core.match :refer [match]]
             [clojure.string :refer [join]])
   (:use [clj-snake.utils]))
 
 (set! *warn-on-reflection* true)
-
-(defn terminal ^Terminal []
-  (-> (TerminalBuilder/builder)
-      (.jansi true)
-      (.build)))
-
-(defn char-reader ^BindingReader [^Terminal terminal]
-  (BindingReader. (-> terminal .reader)))
-
-(def ^BindingReader key-reader (char-reader (terminal)))
-
-(defn key-inputs []
-  (flush)
-  (-> key-reader .readCharacter char))
-
-(defn to-async [source]
-  (-> source
-      (op/subscribeOn (Schedulers/io))
-      (op/observeOn (Schedulers/single))))
-
-(defn repeat-latest-on-interval 
-  ([source delay unit]
-    (repeat-latest-on-interval source delay unit (Schedulers/io)))
-  ([source delay unit ^Scheduler scheduler]
-    (let [sub (fn [^ObservableEmitter e]
-                (let [dsp (atom (Disposable/empty))
-                      nxt (fn nxt [value]
-                            (when (not (.isDisposed e))
-                              (.onNext e value)
-                              (.dispose ^Disposable @dsp)
-                              (reset! dsp (.scheduleDirect scheduler (fns/runnable #(nxt value)) delay unit))))
-                      err (fn err [error]
-                            (.dispose ^Disposable @dsp)
-                            (.onError e error))
-                      com (fn com []
-                            (.dispose ^Disposable @dsp)
-                            (.onComplete e))]
-                (-> source (op/subscribe nxt err com))))]
-    (-> Observable (rx/create sub)))))
 
 (defn filter-opposite [prev curr]
   (match [prev curr]
@@ -121,15 +77,14 @@
 
 (defn update-state [state direction]
   (-> state
-      (assoc :snake (grow state direction))
-      (assoc :food  (food state))))
+      (update-map :snake grow direction)
+      (update-map :food  food)))
 
 (defn initial-state [rows cols]
   {:rows  rows
    :cols  cols
    :snake [[0 1] [0 0]] ;; First element is snake head.
-   :food  (pick-rand-pos rows cols)
-   :ate?  false})
+   :food  (pick-rand-pos rows cols)})
 
 (defn snake-game [rows cols speed]
   (let [initial (initial-state rows cols)]
--- src/clj_snake/utils.clj
+++ src/clj_snake/utils.clj
@@ -1,6 +1,56 @@
-(ns clj-snake.utils)
+(ns clj-snake.utils
+  (:import [org.jline.terminal TerminalBuilder Terminal]
+           [org.jline.keymap   BindingReader]
+           [io.reactivex.rxjava3.core Observable Scheduler ObservableEmitter]
+           [io.reactivex.rxjava3.schedulers Schedulers]
+           [io.reactivex.rxjava3.disposables Disposable]
+           [java.util Collection])
+  (:require [rx-clojure.statics   :as rx]
+            [rx-clojure.operators :as op]
+            [rx-clojure.functions :as fns]))
 
-(defn has? [coll val]
+(set! *warn-on-reflection* true)
+
+(defn terminal ^Terminal []
+  (-> (TerminalBuilder/builder)
+      (.jansi true)
+      (.build)))
+
+(defn char-reader ^BindingReader [^Terminal terminal]
+  (BindingReader. (-> terminal .reader)))
+
+(def ^BindingReader key-reader (char-reader (terminal)))
+
+(defn key-inputs []
+  (flush)
+  (-> key-reader .readCharacter char))
+
+(defn to-async [source]
+  (-> source
+      (op/subscribeOn (Schedulers/io))
+      (op/observeOn (Schedulers/single))))
+
+(defn repeat-latest-on-interval 
+  ([source delay unit]
+    (repeat-latest-on-interval source delay unit (Schedulers/io)))
+  ([source delay unit ^Scheduler scheduler]
+    (let [sub (fn [^ObservableEmitter e]
+                (let [dsp (atom (Disposable/empty))
+                      nxt (fn nxt [value]
+                            (when (not (.isDisposed e))
+                              (.onNext e value)
+                              (.dispose ^Disposable @dsp)
+                              (reset! dsp (.scheduleDirect scheduler (fns/runnable #(nxt value)) delay unit))))
+                      err (fn err [error]
+                            (.dispose ^Disposable @dsp)
+                            (.onError e error))
+                      com (fn com []
+                            (.dispose ^Disposable @dsp)
+                            (.onComplete e))]
+                (-> source (op/subscribe nxt err com))))]
+    (-> Observable (rx/create sub)))))
+
+(defn has? [^Collection coll val]
   (.contains coll val))
 
 (defn add-vec [a b]
@@ -8,3 +58,6 @@
 
 (defn in-range? [u l v]
     (and (>= v u) (< v l)))
+
+(defn update-map [m k f & args]
+  (assoc m k (apply f (cons m args))))
